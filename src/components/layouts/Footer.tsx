5af919c5677bc3fb736a12e1db47821c4ae63e31
Shohidul Arifin <110107262+arifin1987@users.noreply.github.com>
2024-05-07 01:28:36 -0700

    navbar and footer added

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,7 +1,37 @@
 const Footer = () => {
   return (
-    <div>
-      <h1>This is a footer</h1>
+    <div className="md:flex justify-between border-y-2 py-4 my-4">
+      <h1 className="text-3xl font-semibold ">
+        <span className="text-blue-500">D</span>isaster{" "}
+        <span className="text-red-600">R</span>elief
+      </h1>
+      <div>
+        <h1 className="text-2xl font-semibold">Fundraise for</h1>
+        <p>Medical</p>
+        <p>Emergency</p>
+        <p>Memorial</p>
+        <p>Education</p>
+        <p>Non Profit</p>
+        <p>Crisis relief</p>
+      </div>
+      <div>
+        <h1 className="text-2xl font-semibold">Learn More</h1>
+        <p>How disaster relief works</p>
+        <p>Common Questions</p>
+        <p>Success Stories</p>
+        <p>Supported Countries</p>
+        <p>Charity Fundraising</p>
+        <p>Pricing</p>
+      </div>
+      <div>
+        <h1 className="text-2xl font-semibold">Resources</h1>
+        <p>Help Center</p>
+        <p>Blog</p>
+        <p>Disaster Relief Strories</p>
+        <p>Newsroom</p>
+        <p>Careers</p>
+        <p>More Resources</p>
+      </div>
     </div>
   );
 };
--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -1,8 +1,19 @@
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
   return (
-    <div>
-      <h1>This is navbar</h1>
-    </div>
+    <nav className="w-full h-full bg-slate-200 md:flex justify-between items-center p-6 ">
+      <h1 className="text-3xl font-semibold ">
+        <span className="text-blue-500">D</span>isaster{" "}
+        <span className="text-red-600">R</span>elief
+      </h1>
+      <ul className="space-x-5">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/">All Donations</NavLink>
+        <NavLink to="/">Donation Detail</NavLink>
+        <NavLink to="/">Login</NavLink>
+      </ul>
+    </nav>
   );
 };
 
