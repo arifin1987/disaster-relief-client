907e78b3b5dccb964cf2545106605aa6836e4ff0
Shohidul Arifin <110107262+arifin1987@users.noreply.github.com>
2024-05-14 23:42:49 -0700

    view details text and color edited

--- src/pages/Home/Top Donations/SingleTopDonations.tsx
+++ src/pages/Home/Top Donations/SingleTopDonations.tsx
@@ -13,9 +13,9 @@ interface productType {
 const SingleTopDonations = ({ item }: productType) => {
   const { image, title, amount, _id } = item;
   return (
-    <div className=" border-2 border-slate-950 p-4">
-      <img className="h-[400px]" src={image} alt="" />
-      <h1 className="text-green-900">{title}</h1>
+    <div className=" border-2 border-slate-950 p-4 rounded-md">
+      <img className="h-[400px] rounded" src={image} alt="" />
+      <h1 className="text-blue-600">{title}</h1>
       <p className="text-red-500 font-bold">${amount}</p>
       <Link to={`/donations/${_id}`}>
         <Button className="bg-blue-500">View Details</Button>{" "}
--- src/pages/ViewDetails/ViewDetails.tsx
+++ src/pages/ViewDetails/ViewDetails.tsx
@@ -12,10 +12,10 @@ const ViewDetails = () => {
 
   return (
     <div className="md:flex gap-4 justify-center items-center my-4">
-      <img className="h-[400px]" src={image} alt="" />
+      <img className="h-[400px] rounded-md" src={image} alt="" />
       <div>
-        <h1 className="text-green-900">{title}</h1>
-        <p>{details}</p>
+        <h1 className="text-blue-600 text-2xl">{title}</h1>
+        <p className="text-green-500 w-2/3">{details}</p>
         <p className="text-red-500 font-bold">${amount}</p>
       </div>
     </div>
